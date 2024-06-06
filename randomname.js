3e0efbeb7f1979542ae2a0d55542dee88006751a
Etyala Rahul <162606708+EtyalaRahul@users.noreply.github.com>
2024-06-06 21:26:58 +0530

    Add files via upload

---
+++ randomname.js
@@ -0,0 +1,22 @@
+for(let i=0;i<10000;i++){
+ let r=Math.random();
+ if(r<0.10)
+    console.log("rahul");
+else if(r<0.20)
+    console.log("Sri vardhan");
+else if(r<0.30)
+    console.log("harry");
+else if(r<0.40)
+    console.log("sradha");
+else if(r<0.50)
+    console.log("Harshitha");
+else if(r<0.60)
+    console.log("guru")
+else if(r<0.70)
+    console.log("ashrith");
+else if(r<0.80)
+    console.log("himalaya");
+else if(r<0.90)
+    console.log("tulip")
+
+}
